feacb287c64566165f18910a3a41debfb0a5ad67
Hari Om Singh <pantrokbazz@gmail.com>
2026-09-05 21:19:18 +0530

    feat: connect flutter frontend to live render backend url

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String baseUrl = 'http://localhost:8080/api/v1';
+  static const String baseUrl = 'https://ebook-backend-imn2.onrender.com/api/v1';
 
   // Auth
   static const String login = '/auth/login';
